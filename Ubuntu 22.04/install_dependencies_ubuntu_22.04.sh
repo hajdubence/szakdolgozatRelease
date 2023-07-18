87f538a22f50809832381794e8535954585a5acf
Bence <hajdubence2001@gmail.com>
2023-07-18 21:26:59 +0200

    Ubuntu

--- Ubuntu 22.04/install_dependencies_ubuntu_22.04.sh
+++ Ubuntu 22.04/install_dependencies_ubuntu_22.04.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#g++ GLEW FLFW GLM assimp pkg-config
-apt install g++ libglew2.2 libglew-dev libglfw3 libglfw3-dev libglm-dev assimp-utils libassimp-dev libassimp5 pkg-config
+#GLEW GLFW assimp
+apt install libglew2.2 libglfw3 libassimp5
